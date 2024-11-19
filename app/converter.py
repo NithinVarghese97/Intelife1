bb25c4f3e1da11cc2a9a63351b7a909af72cf26d
Divrr <96827300+Divrr@users.noreply.github.com>
2024-11-19 12:36:22 +0800

    added text/output.txt failsafe

--- app/converter.py
+++ app/converter.py
@@ -4,8 +4,17 @@ from context_generation import extract_text_from_pdf, write_string_to_file
 from translation import Wrapper
 from pdf_generation import replace_pdf_text
 import threading
+import os
   
 def convert(pdf_path):
+    # Create the directory if it doesn't exist
+    os.makedirs(os.path.dirname('app/text/output.txt'), exist_ok=True)
+
+    # Create the file if it doesn't exist
+    if not os.path.exists('app/text/output.txt'):
+        with open('app/text/output.txt', 'w') as f:
+            pass
+    
     txt_path = 'app/text/output.txt'
     text, locations = extract_text_from_pdf(pdf_path, ignore_small_font=False)
     write_string_to_file(" ".join(text), txt_path)
